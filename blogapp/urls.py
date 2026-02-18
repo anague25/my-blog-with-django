5a71cffe43160b660e520e666344920e2a8803f1
roosvelt <anaguesonnaroosvelt@gmail.com>
2026-02-18 18:10:31 +0100

    authentification systeme

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from blogapp import views
 
 app_name = 'blogapp'
 
 urlpatterns = [
-    
+path('', views.home_view, name='home'),
+path('connexion/', views.connexion_view, name='connexion'),
+path('register/', views.register_view, name='register'),
+path('logout/', views.logout_view, name='logout'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import logout, login
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+
+def home_view(request):
+    return render(request, "home.html")
+
+
+def connexion_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request,data=request.POST)
+        form.is_valid()
+        user = form.get_user()
+        if user is not None:
+            login(request, user)
+            return redirect("blogapp:home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "connexion.html", {"form": form})
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("blogapp:home")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
+
+
+# def register_view(request):
+#    if request.method == 'POST':
+#     form = UserCreationForm(request.POST)
+#     if form.is_valid():
+#         form.save()
+#         username = form.cleaned_data.get('username')
+#         password = form.cleaned_data.get('password1')
+#         user = authenticate(username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return render(request,'home.html')
+
+
+#     return render(request,'register.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect("blogapp:home")
